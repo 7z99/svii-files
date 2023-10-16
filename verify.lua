62ae635bcd18a16bde67eda9f8255e32bd6d2773
7z99 <104286985+7z99@users.noreply.github.com>
2023-10-16 03:24:25 -0300

    Update verify.lua

--- verify.lua
+++ verify.lua
@@ -1,7 +1,7 @@
 local module = {
     {
         title = 'Please link your Roblox account.';
-        description = 'Type `/verify` in this channel to begin the verification process, which links your Roblox account to your Discord account.\n\nVerification is done through <@426537812993638400>.';
+        description = 'Type `/verify` in <#1163361728269127790> to begin the verification process, which links your Roblox account to your Discord account.\n\nVerification is done through <@426537812993638400>.';
         color = discordia.Color.fromHSV(0, .5, 1).value
     };
 }
