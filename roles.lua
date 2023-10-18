b645a952b5cf8cbbefa9ca1251a2941da6d6e31b
7z99 <104286985+7z99@users.noreply.github.com>
2023-10-17 22:31:16 -0300

    Create roles.lua

---
+++ roles.lua
@@ -0,0 +1,16 @@
+local module = {
+    {
+        title = 'Would you like any name colours?';
+        description = '<:red:1164012147576143925> - Red\
+<:orange:1164012145307046008> - Orange\
+<:yellow:1164012152391204925> - Yellow\
+<:green:1164012144333946931> - Green\
+<:teal:1164012149283233945> - Teal\
+<:blue:1164012141691535420> - Blue\
+<:purple:1164012146099761234> - Purple\
+<:white:1164012150268899369> - White\
+<:default:1164012143465734154>\ - Default (auto role colour)';
+    };
+}
+
+return module
