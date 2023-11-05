be4f9b24d286ec2b76ef0041df3984c7c4989519
7z99 <104286985+7z99@users.noreply.github.com>
2023-11-05 18:13:26 -0400

    Create rolesPings.lua

---
+++ rolesPings.lua
@@ -0,0 +1,9 @@
+local module = {
+    {
+        title = 'Would you like to receive any pings?';
+        description = 'DevForum pings come from the [announcement channels on the developer forum](https://devforum.roblox.com/c/updates/45).\
+The other pings are done manually.'
+    };
+}
+
+return module
