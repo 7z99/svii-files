1de35cf833af196fcb4f6b25463a138be0d44865
7z99 <104286985+7z99@users.noreply.github.com>
2023-11-05 20:10:09 -0400

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,1 +1,50 @@
-a
+local module = {
+    {
+        title = 'Introduction to the server';
+        description = 'Welcome to our server! We\'re glad to have you here. This server is a general development server for general game development and UGC items.\
+\
+This channel will give you a brief overview of our server and how it works.';
+    };
+  {
+    title = 'Channels';
+    description = '#### Important\
+<#1162171113460215938> - This channel.\
+<#1161872087900749965> - The rules channel.\
+<#1162172174493286470> - Where you can receive pings for general server announcements.\
+<#1161919924697047101> - Where you can assign yourself some roles, such as name colours and pings.\
+
+#### DevForum Updates\
+<#1170630771170615376> - Where new posts from the Developer Forum\'s [announcements channel](https://devforum.roblox.com/c/updates/announcements/36) will be posted.\
+<#1170630842675122187> - Where new posts from the Developer Forum\'s [news and alerts channel](https://devforum.roblox.com/c/updates/news-alerts/193) will be posted.\
+<#1170630878939054160> - Where new posts from the Developer Forum\'s [release notes channel](https://devforum.roblox.com/c/updates/release-notes/62) will be posted.\
+<#1170630904251699311> - Where new posts from the Developer Forum\'s [community and events channel](https://devforum.roblox.com/c/updates/community/90) will be posted.\
+\
+#### Verify\
+<#1162174674243964979> - Where information relating to verifying your Roblox account is posted.\
+<#1163361728269127790> - Where you can verify your Roblox account. Note that verifying your Roblox account through Bloxlink is required in order to access the rest of the server.\
+\
+#### General\
+<#1170201489201954837> - Where general discourse happens.\
+<#1162188859593994382> - Where you can share and receive music recommendations.\
+<#1163354141221994588> - Where bot commands should go.\
+\
+#### Development\
+<#1164005831990321245> - Where the staff might decide to showcase some creations or new technology announced by Roblox.\
+<#1170155971188887722> - Where anyone can showcase their creations.\
+<#1164006097019994112> - Where you can discuss development on Roblox.\
+<#1170157317912805386> - Where you can ask for help on any subject of Roblox development.\
+<#1170157084483006464> - Where you can ask for guidance related to scripting.'
+  };
+  {
+    title = 'Roles';
+    description = '#### Staff Roles\
+<@1161882198601900102> - This role is given to the server\'s owner (<@686411634004066343>\
+<@1161871403142873128> - This role is given to members of this server\'s administration team.\
+<@1170551494995628103> - This role is given to members of this server\'s moderation team.\
+#### Ranks\
+<@&1161871381735149618> - This role is given to friends of <@686411634004066343>.\
+<@&1170563694581796915> - This role is given to members of the UGC program.\
+<@&1162178409410084965> - This role is automatically given after verifying your Roblox account.\
+<@&1162179131967021226> - This role is automatically given when you join the server and haven\'t linked your Roblox account.'
+  }
+}
