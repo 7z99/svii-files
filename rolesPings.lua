7aa0296b7400d710371babf3151a5bdca42513ad
7z99 <104286985+7z99@users.noreply.github.com>
2023-11-13 20:42:01 -0400

    Update rolesPings.lua

--- rolesPings.lua
+++ rolesPings.lua
@@ -2,7 +2,7 @@ local module = {
     {
         title = 'Would you like to receive any pings?';
         description = 'DevForum pings come from the [announcement channels on the developer forum](https://devforum.roblox.com/c/updates/45).\
-The other pings are done manually.'
+The other pings are done manually.\n\n🛠 - This role will be pinged whenever a new announcement related to development is posted by the staff.\n👖 - This role will be pinged whenever new UGC item(s) are posted.\n📅 - This role will be pinged whenever a new server event is hosted. This includes game nights and free UGC events.\n🗨 - This role will be pinged whenever a new server announcement is made. This includes new server announcements and polls.\n❓ - This role will be pinged whenever a new question of the day (QOTD) is posted.'
     };
 }
 
